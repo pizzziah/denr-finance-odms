4a37359f0ffc137d16c32bca84c8a4c89daea249
Keziah Mae S. Tingga-an <keziahmaeee@gmail.com>
2026-06-17 09:15:02 +0800

    convert to boostrap notation

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -1,27 +1,24 @@
-<header class="topbar">
-
-    <div class="topbar-left">
+<header class="d-flex justify-content-between align-items-center ">
+    <div class="d-flex align-items-center gap-3">
     <!-- TO-DO: update once database is ok { $pageTitle >-->
-        <h2>Dashboard</h2>
+        <h3 class="fw-bold" style="color: var(--primary);">
+            Tab
+        </h3>
     </div>
 
-    <div class="topbar-right">
-        <button class="btn position-relative">
-            <i class="bi bi-bell-fill fs-5"></i>
-            <span
-                class="position-absolute top-0 start-100 translate-middle p-1 bg-danger border border-dark rounded-circle">
-            </span>
-        </button>
-
-        <div class="ms-3 text-end">
-            <h2 class="mb-0 fw-bold">
-               <!-- TO-DO: update once database is ok Auth::user()->name }}</h2>-->
-                JUAN D. DELA CRUZ
-            </h2>
-            <!-- TO-DO: update once database is ok Auth::user()->name }}</h4>-->
-            <h4 class="text-muted">
-                <i>Accounting</i>
-            </h4>
+    <div class="d-flex align-items-center gap-3">
+        <div>
+            <button class="d-flex flex-row fw-bold fs-5 gap-2 "
+                style="background-color: var(--background); color: var(--text-dark); border:none; border-radius: 8px;">
+                <i class="bi bi-app-indicator"></i>
+                Notifications
+            </button>
         </div>
+        <hr style="border-right-width: 1px; border-right-style: solid; border-right-color: var(--text-dark); height: 30px;">
+        <!-- TO-DO: update once database is ok Auth::user()->name }}</h2>-->
+        <h5 class="p-3 fw-bold" 
+            style="color: var(--primary); background-color: var(--secondary-variant); border: 1px solid var(--primary); border-radius: 8px; border: 1px solid var(--primary);">
+            Accounting Department
+        </h5>
     </div>
 </header>
