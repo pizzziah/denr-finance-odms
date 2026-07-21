b16c063aa8cecb86eb36a489322afb4a58d26d68
Kyla Ferrer <kyla5952ferrer@gmail.com>
2026-07-21 22:31:27 +0800

    update: subtab

--- resources/views/layouts/subtab.blade.php
+++ resources/views/layouts/subtab.blade.php
@@ -13,13 +13,14 @@
 
   {{-- BUDGET --}}
   @if(strtolower(Auth::user()->role) == 'budget')
-    <x-subtab-link status="all" label="All"/>
-    <x-subtab-link status="for_obligation" label="For Obligation"/>
-    <x-subtab-link status="Processing" label="Processing"/>
-    <x-subtab-link status="Returned to End User" label="Returned to End User"/>
-    <x-subtab-link status="returned_by_accounting" label="Returned by Accounting"/>
-    <x-subtab-link status="forwarded_to_accounting" label="Forwarded to Accounting"/>
-    <x-subtab-link status="Paid" label="Paid"/>
-    <x-subtab-link status="Pending" label="Pending"/>
+      <x-subtab-link status="all" label="All"/>
+      <x-subtab-link status="pending" label="Pending"/>
+      <x-subtab-link status="processing" label="Processing"/>
+      <x-subtab-link status="for_obligation" label="For Obligation"/>
+      <x-subtab-link status="returned_to_end_user" label="Returned to End User"/>
+      <x-subtab-link status="returned_by_accounting" label="Returned by Accounting"/>
+      <x-subtab-link status="forwarded_to_accounting" label="Forwarded to Accounting"/>
+      <x-subtab-link status="paid" label="Paid"/>
+      <x-subtab-link status="Cancelled" label="Cancelled"/>
   @endif
 </div>
